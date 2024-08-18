0f7916f97fe9c8557151bc872d4fe18f7c434fcc
Tony James <3128081+tonyjames@users.noreply.github.com>
2024-08-18 16:43:02 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,16 @@
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
-INCLUDED_PACKAGES=$(jq -r ".include | (.containers[], .fonts[], .misc[], .multimedia[], .virtualization[])" /shared/packages.json)
-EXCLUDED_PACKAGES=$(jq -r ".exclude | (.misc[], .multimedia[])" /shared/packages.json)
 
 rpm-ostree install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
+EXCLUDED_PACKAGES=$(jq -r ".exclude | (.multimedia[])" /shared/packages.json)
 rpm-ostree override remove ${EXCLUDED_PACKAGES}
 
+INCLUDED_PACKAGES=$(jq -r ".include | (.containers[], .fonts[], .misc[], .multimedia[])" /shared/packages.json)
 rpm-ostree install ${INCLUDED_PACKAGES}
+
+EXCLUDED_PACKAGES=$(jq -r ".exclude | (.misc[])" /shared/packages.json)
+rpm-ostree override remove ${EXCLUDED_PACKAGES}
