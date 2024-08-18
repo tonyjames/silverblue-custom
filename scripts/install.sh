7c8275aaf846041e798f9b9fe7055504ae55d37d
Tony James <tony@x1.home.arpa>
2024-08-18 19:53:04 -0400

    Updating some things

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,11 +8,6 @@ rpm-ostree install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-rpm --import https://dl.google.com/linux/linux_signing_key.pub
-rpm --import https://pkgs.tailscale.com/stable/fedora/repo.gpg
-
 EXCLUDED_PACKAGES=$(jq -r ".exclude | (.multimedia[])" /shared/packages.json)
 rpm-ostree override remove ${EXCLUDED_PACKAGES}
 
